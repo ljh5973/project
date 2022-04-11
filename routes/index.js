4760ae65ad4953f47abed94035863708cf76ea90
ljh5973 <ljh6650@naver.com>
2022-04-12 00:13:39 +0900

    googlelogin 하는중

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,16 @@ const router = express();
 router.get('/test', (req, res) => {
     res.send({test: "Test"});
 });
-router.post('/api/users/register', (req, res) => {
-    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
-    // 그것들을 데이터 베이스에 넣어준다.
+// router.post('/api/users/register', (req, res) => {
+//     // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+//     // 그것들을 데이터 베이스에 넣어준다.
     
-    console.log('api test')
-    connection.query("select * from users", (err, rows, fields) => {
-        res.send(rows);
-        console.log(rows);
-    });
-})
+//     console.log('api test')
+//     connection.query("select * from users", (err, rows, fields) => {
+//         res.send(rows);
+//         console.log(rows);
+//     });
+// })
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,66 @@ const port = process.env.PORT || 5000;
 const api = require("./routes/index");
 const cors = require('cors');
 
+const {google} = require("googleapis");
+var googleClient =require('./config/google.json');
+
+const googleConfig
+={
+    clientId:googleClient.web.client_id,
+    clientSecret:googleClient.web.client_secret,
+    redirect:googleClient.web.redirect_uris[0]
+}
+
+const scopes = [
+    'https://www.googleapis.com/auth/plus.me'
+  ];
+   
+   
+  const oauth2Client =new google.auth.OAuth2(
+    googleConfig.clientId,
+    googleConfig.clientSecret,
+    googleConfig.redirect
+  );
+
+//   google + api 를 사용하기 위해 google+ api 에 대한 정보 입력
+
+const url = oauth2Client.generateAuthUrl({
+    access_type:'offline',
+    scope:scopes
+});
+function getGooglePlusApi(auth){
+    return google.plus({version:'v1',auth});
+}
+
+// 실질적으로 로그인해서 정보를 불러올 코드 작성
+// 리프레시토큰 엑세스토큰 displayName과 id를 얻어와본다.
+async function googleLogin(code){
+    const{tokens}=await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+    oauth2Client.on('tokens', (token)=>{
+        if(token.refresh_token){
+            console.log("리프레시 토큰 : ",tokens.refresh_token);
+        }
+        console.log("액세스 토큰 : ", tokens.access_token);
+    });
+    const plus =getGooglePlusApi(oauth2Client);
+    const res=await plus.people.get({userId:'me'});
+    console.log(`Hello ${res.data.displayName}! ${res.data.id}`);
+    return res.data.displayName;
+}
+
+app.get('/googleLogin', function(req,res){
+    res.redirect(url);
+});
+
+app.get("/auth/google/callback", async function(req,res){
+    const displayName=await googleLogin(req.query.code);
+    console.log(displayName);
+
+    res.redirect("http://localhost:3000");
+});
+  
+  
 app.use(cors());
 //api 미들웨어 등록
 app.use('/api', api);
